c047fde7d873a98aa807bb409424a5ca5e741326
shauryaj <shauryaj@cs.washington.edu>
2021-04-24 12:16:54 -0700

    jdbc

---
+++ backend/src/main/java/pocketgrocer/Query.java
@@ -0,0 +1,50 @@
+package src.main.java.pocketgrocer;
+
+import java.io.*;
+import java.sql.*;
+import java.util.*;
+import java.util.logging.Logger;
+import java.security.*;
+import java.security.spec.*;
+import javax.crypto.*;
+import javax.crypto.spec.*;
+
+public class Query {
+    // DB Connection
+    private Connection conn;
+
+    private static final Logger log;
+
+    static {
+        System.setProperty("java.util.logging.SimpleFormatter.format", "[%4$-7s] %5$s %n");
+        log =Logger.getLogger(Query.class.getName());
+    }
+
+    public static void main(String[] args) throws Exception {
+        log.info("Loading application properties");
+        Properties properties = new Properties();
+        properties.load(Query.class.getClassLoader().getResourceAsStream("application.properties"));
+
+        log.info("Connecting to the database");
+        Connection connection = DriverManager.getConnection(properties.getProperty("url"), properties);
+        log.info("Database connection test: " + connection.getCatalog());
+
+        log.info("Create database schema");
+        Scanner scanner = new Scanner(Query.class.getClassLoader().getResourceAsStream("schema.sql"));
+        Statement statement = connection.createStatement();
+        while (scanner.hasNextLine()) {
+            statement.execute(scanner.nextLine());
+        }
+
+        log.info("Closing database connection");
+        connection.close();
+    }
+
+    // function to close the connection to a SQL database
+    public void closeConnection() throws SQLException {
+        conn.close();
+    }
+
+
+
+}
