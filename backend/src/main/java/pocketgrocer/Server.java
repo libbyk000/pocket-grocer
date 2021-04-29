6290fa0c5878423539e17c8319acaaa3c57f4cb8
shauryaj <shauryaj@cs.washington.edu>
2021-04-29 14:12:41 -0700

    update server

--- backend/src/main/java/pocketgrocer/Server.java
+++ backend/src/main/java/pocketgrocer/Server.java
@@ -6,7 +6,7 @@ import static spark.Spark.*;
 public class Server {
     public static void main(String[] args) {
         // Java serialization/deserialization library to convert Java Objects into JSON and back
-        // https://github.com/google/gson
+        // https://github.co`m/google/gson
         Gson gson = new Gson();
         Query query = new Query();
 
@@ -18,7 +18,6 @@ public class Server {
             String password = request.queryParams("Password");
 
             try {
-
                 // do sth here
                 if (true) { // if user already exisst in db
                     // private static final String CHECK_FLIGHT_CAPACITY = "SELECT capacity FROM Flights WHERE fid = ?";
