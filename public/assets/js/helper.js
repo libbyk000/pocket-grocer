fab53d861911ee378c3c0c42aadcd0be21c6bedb
Fadel Shtiui <fadelshtiui@gmail.com>
2021-05-10 22:05:12 -0700

    add comments

--- public/assets/js/helper.js
+++ public/assets/js/helper.js
@@ -1,4 +1,7 @@
+// base server URL
 const BASE_URL = "http://localhost:4567"
+
+// error messages
 const USER_DNE_ERR = "Your username was not found. Please login again."
 const GENERIC_SERVER_ERR = "Something went wrong on our end. Please try again later."
 const ITEM_DNE_ERR = "This item no longer exists."
@@ -9,26 +12,54 @@ const USER_TAKEN_ERR = "This username is already in use."
 const ALREADY_IN_GROUP_ERR = "You are already in a group. Please leave before trying to join a new one."
 const INCORRECT_PASSWORD_ERR = "Your username and password do not match."
 
+// helper functions
+
+/**
+ * returns HTML element with the given ID
+ * 
+ * @param {string} id - id of html element
+ * @returns {object} - DOM object
+ */
 function id(id) {
     return document.getElementById(id)
 }
 
+/**
+ * returns first HTML element that matches the given CSS selector
+ * 
+ * @param {string} selector - css selector
+ * @returns {object} - DOM object
+ */
 function qs(selector) {
     return document.querySelector(selector)
 }
 
+/**
+ * returns an array of HTML elements that match the given CSS selector
+ * 
+ * @param {string} selector - css selector
+ * @returns 
+ */
 function qsa(selector) {
     return document.querySelectorAll(selector)
 }
 
+/**
+ * returns a reference to a new HTML element of the given tag
+ * 
+ * @param {string} tag - valid HTML tag
+ * @returns {object} - HTML element
+ */
 function gen(tag) {
     return document.createElement(tag)
 }
 
-function qs(selector) {
-    return document.querySelector(selector)
-}
-
+/**
+ * returns the cookie with the name userName
+ * returns empty string if cookie is not set
+ * 
+ * @returns {string} - username cookie
+ */
 function getUserName() {
     let cookies = document.cookie.split(";");
     let userName = "";
@@ -41,6 +72,9 @@ function getUserName() {
     return userName;
 }
 
+/**
+ * clears all cookies, and navigates to the home page
+ */
 function signOut() {
     let cookies = document.cookie.split(";");
     for (let i = 0; i < cookies.length; i++) {
@@ -52,6 +86,16 @@ function signOut() {
     window.location = "/";
 }
 
+/**
+ * returns an appropriate icon based on the number of days until
+ * expiration of an item.
+ * <=0 days - red warning icon
+ * <=3 days - yellow warning icon
+ * >=4 days - green check mark
+ * 
+ * @param {number} daysRemaining - integer number of days
+ * @returns {object} - icon HTML element
+ */
 function generateIcon(daysRemaining) {
     let icon = gen('i');
     icon.classList.add('fas');
@@ -65,6 +109,14 @@ function generateIcon(daysRemaining) {
     return icon;
 }
 
+/**
+ * converts FormData object into JSON object
+ * takes the entries and values from the FormData object and sets them
+ * as the top-level fields and values respectively in the JSON object.
+ * 
+ * @param {object} formData - object representing HTML form element
+ * @returns {JSON} - key value pairs
+ */
 function generateRequestBody(formData) {
     let object = {};
     formData.forEach(function(value, key) {
