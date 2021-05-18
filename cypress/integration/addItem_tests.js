34ff18f62122da1dea20a76c0ca2bac0fe5d2c8a
Libby Knell <libbyknell@Libbys-MacBook-Air.local>
2021-05-17 22:02:20 -0700

    added storage location, quantity, bought recently tests to AddItem test suite

--- cypress/integration/addItem_tests.js
+++ cypress/integration/addItem_tests.js
@@ -5,6 +5,19 @@ context('addItem Tests', () => {
         cy.visit('https://pocket-grocer-403.azurewebsites.net/addItem.html')
     })
 
+    // BOUGHT RECENTLY
+    it ('Bought recently checked', function () {
+        // .check() accepts a value argument
+        cy.get('#yes')
+        .check({ force: true })
+        .should('be.checked')
+
+        cy.get('#no')
+        .check({ force: true })
+        .should('be.checked')
+    })
+
+    // ITEM NAME
     it ('Ensure item name can be typed', function () {
         // at first, no option should be selected
         cy.get('#item-name-input')
@@ -16,6 +29,65 @@ context('addItem Tests', () => {
         .should('have.value', 'apple')
     })
 
+    // QUANTITY
+    it ('Increment quantity - valid', function () {
+        // Start value
+        cy.get('#quantity')
+        .should('have.value', 1)
+
+        // increment
+        cy.get('#plus')
+        .click()
+
+        cy.get('#quantity')
+        .should('have.value', 2)
+    })
+
+    it ('Decrement quantity - invalid', function () {
+        // Start value
+        cy.get('#quantity')
+        .should('have.value', 1)
+
+        cy.get('#minus')
+        .click()
+    
+        cy.get('#quantity')
+        .should('have.value', 1)
+    })
+
+    it ('Increment then decrement quantity - valid', function () {
+        // Start value
+        cy.get('#quantity')
+        .should('have.value', 1)
+
+        // increment
+        cy.get('#plus')
+        .click()
+
+        cy.get('#quantity')
+        .should('have.value', 2)
+
+        cy.get('#minus')
+        .click()
+    
+        cy.get('#quantity')
+        .should('have.value', 1)
+    })
+
+
+    // STORAGE LOCATION 
+    it ('Storage location checked', function () {
+        // .check() accepts a value argument
+        cy.get('#pantry-tag')
+        .check({ force: true })
+        .should('be.checked')
+
+        cy.get('#refrigerator-tag')
+        .check({ force: true })
+        .should('be.checked')
+    })
+
+    // ITEM CATEGORY
     it ('Ensure item category can be selected', function () {
         // at first, no option should be selected
         cy.get('#item-category')
