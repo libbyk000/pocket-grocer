188a6491aed506963956fc0caefd92dd5a881b42
Libby Knell <libbyknell@Libbys-MacBook-Air.local>
2021-05-17 20:33:05 -0700

    moved examples out of testing folder

--- cypress/integration/addItem_tests.js
+++ cypress/integration/addItem_tests.js
@@ -5,37 +5,25 @@ context('addItem Tests', () => {
         cy.visit('https://pocket-grocer-403.azurewebsites.net/addItem.html')
     })
 
-    it('all radios can be checked', function () {
-        cy.get('[type="radio"]')
-        .not('[disabled]')
-        .check().should('be.checked')
-    })
-
-    it('all radios can be unchecked', function () {
-        cy.get('[type="radio"]')
-        .not('[disabled]')
-        .uncheck().should('not.be.checked')
-    })
-
-    it ('Ensure item name can be selected', function () {
+    it ('Ensure item name can be typed', function () {
         // at first, no option should be selected
         cy.get('#item-name-input')
         .should('have.value', '')
 
-        cy.get('#item-name-input').select('apple')
         // confirm the apple was selected
-        cy.get('#item-name-input').should('have.value', 'apple')
+        cy.get('#item-name-input')
+        .type('apple')
+        .should('have.value', 'apple')
     })
 
     it ('Ensure item category can be selected', function () {
-        // at first, no option should be selected, rep by -
+        // at first, no option should be selected
         cy.get('#item-category')
-        .should('have.value', '-')
+        .should('have.value', '')
 
         
         // Select option(s) with matching text content
         cy.get('#item-category').select('Beverages')
-
         cy.get('#item-category').should('have.value', 'Beverages')
     })
 })
